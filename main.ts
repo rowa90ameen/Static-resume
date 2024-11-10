a3e7279a23d872d3d51aa47406f88c1b01546184
rowa90ameen <159520497+rowa90ameen@users.noreply.github.com>
2024-11-10 19:46:04 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,9 @@
+const button = document.getElementById('toggle-skills') as HTMLButtonElement
+const skills = document.getElementById('skills') as HTMLElement
+button.addEventListener('click', ()=> {
+    if(skills.style.display === 'none') {
+        skills.style.display = 'block'
+    }else{
+        skills.style.display = 'none'
+    }
+});
